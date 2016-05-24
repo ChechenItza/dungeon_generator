0e448a839bb194ac9efb93138b07a65496bf8189
bbg <artka.kazaktar@gmail.com>
2016-05-24 20:11:52 +0600

    Merged Rand.h and Rand.cpp into a Rand.h header
    Since it's a library and not a standalone application, it seems logical
    to me to get rid of .cpp file and leave just header.

--- Headers/Rand.h
+++ Headers/Rand.h
@@ -1,9 +1,28 @@
 #ifndef RAND_H
 #define RAND_H
 
+#include <random>
+#include <ctime>
+
+//FORWARD DECLARATION
 namespace rnd {
     int randomize(int, int);
     int randomize(int);
+}
+
+//DEFINITON
+std::mt19937 mt(time(NULL));
+
+int rnd::randomize(int min, int max)
+{
+    std::uniform_int_distribution<int> dist_int(min, max);
+    return dist_int(mt);
+}
+
+int rnd::randomize(int max)
+{
+    std::uniform_int_distribution<int> dist_int(0, max);
+    return dist_int(mt);
 }
 
 #endif //RAND_H
